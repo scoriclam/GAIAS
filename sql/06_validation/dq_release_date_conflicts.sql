a5ea11695a99d590161329fc4c16f3e4283b688f
Scott Lamitie <scotthome@Scotts-iMac-4.local>
2026-08-23 17:12:56 -0400

    Extract data quality validation SQL

---
+++ sql/06_validation/dq_release_date_conflicts.sql
@@ -0,0 +1,1 @@
+CREATE VIEW dq_release_date_conflicts AS SELECT game_title, platform_name, release_date AS source_release_date, 'BEFORE_PS5_LAUNCH' AS issue_type FROM std_inventory_curated WHERE ((platform_name = 'PS5') AND (release_date < CAST('2020-11-12' AS DATE)));
